0cdc56436973ed8864375aa6756dae69167e486a
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2016-08-02 06:22:18 +0000

    WDI : add comments

--- cran-wdi/wdi.R
+++ cran-wdi/wdi.R
@@ -1,9 +1,20 @@
+# Load library
 library(WDI)
+
+# Build object
 d = WDI(indicator='NY.GNP.PCAP.CD',country=c('ID'),start=1970,end=2010)
+
+# Data Summary
 summary(d)
+
+# Get first 5 rows
 head(d,5)
+
+# Build plot
 library(ggplot2)
 ggplot(d,aes(year,NY.GNP.PCAP.CD,color=country)) + geom_line() + xlab('Year') +
  ylab('GDI per capita (Atlas Method USD)') + labs(title='GNI Per Capita (USD Atlas Method)')
+
+# Search object
 WDIsearch('gnp')
 
